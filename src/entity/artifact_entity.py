7766bbbbb61904f6e8fbe04a38067680faf12fa8
DevanshRajani25 <devanshrajani25@gmail.com>
2025-05-17 12:37:28 +0530

    Data validation part done

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -1,7 +1,16 @@
+# This file specify what output we need in our artifact(output) folder
 from dataclasses import dataclass
 
 
+# This will give following items in data ingestion folder in artifact folder 
 @dataclass
 class DataIngestionArtifact:
     trained_file_path:str 
     test_file_path:str
+
+# This will give following items in data validation folder in artifact folder 
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    message: str
+    validation_report_file_path: str
